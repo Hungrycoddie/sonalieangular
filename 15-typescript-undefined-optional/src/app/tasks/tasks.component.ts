b879faee6d2d764699cc09bef17ab16be7be3168
Arun raj <106869558+Hungrycoddie@users.noreply.github.com>
2025-02-17 22:02:24 +0530

    ...extra time

--- 15-typescript-undefined-optional/src/app/tasks/tasks.component.ts
+++ 15-typescript-undefined-optional/src/app/tasks/tasks.component.ts
@@ -1,12 +1,25 @@
 import { Component, Input } from '@angular/core';
 
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input() name?: string;
+  @Input() avatar?: string;
+
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
 }
